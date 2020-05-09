17a4e76956faa59fa59cadd56e7b6df05d45f264
dhythm <Yuta.Okada.20@gmail.com>
2020-05-10 00:06:26 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Dictaphone from './Dictaphone';
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
+      <div>react-speech</div>
       <Dictaphone />
     </div>
   );
