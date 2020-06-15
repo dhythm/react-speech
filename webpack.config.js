6904ccca32ff6d4b58094f70e60b468091023381
dhythm <Yuta.Okada.20@gmail.com>
2020-06-16 01:37:58 +0900

    refactor webpack

--- webpack.config.js
+++ webpack.config.js
@@ -24,13 +24,13 @@ module.exports = {
   resolve: { extensions: ['.ts', '.tsx', '.js'] },
   output: {
     path: path.resolve(__dirname, 'dist/'),
-    publicPath: '/dist/',
+    // publicPath: '/dist/',
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: path.join(__dirname, 'dist/'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist/',
+    // publicPath: 'http://localhost:3000/dist/',
     hotOnly: true,
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
